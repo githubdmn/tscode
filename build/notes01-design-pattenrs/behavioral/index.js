f34a77f31149fc76095cc776c45fa1fe94470f5b
githubdmn <dmninbox@gmail.com>
2022-10-12 18:02:33 +0200

    build deleted

--- build/notes01-design-pattenrs/behavioral/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const title = 'Behavioral Design Patterns in TypeScript';
-function default_1() {
-    console.log(`\t\t${title}`);
-}
-exports.default = default_1;
--- build/notes01-design-patterns/creational/prototype-example02.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const title = '->->Protorype example SHAPES\n';
-exports.default = () => {
-    console.log(title);
-};
