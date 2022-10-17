a82935c4b3e98dd5f6f615499b0f03c5a5eee6d9
githubdmn <dmninbox@gmail.com>
2022-10-17 09:34:33 +0200

    Adapter payment example

--- build/notes01-design-patterns/structural/adapter-example02.js
+++ build/notes01-design-patterns/structural/adapter-example02.js
@@ -1,11 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const title = '->->Adapter payment exmaple';
-var PaymentType;
-(function (PaymentType) {
-    PaymentType[PaymentType["ThirdParty"] = 0] = "ThirdParty";
-    PaymentType[PaymentType["Propietary"] = 1] = "Propietary";
-})(PaymentType || (PaymentType = {}));
 class Payment {
     constructor(id, total) {
         this.id = id;
@@ -15,6 +10,47 @@ class Payment {
         console.log(`Proprietary Payment Amount: ${this.total} - ID: ${this.id}`);
     }
 }
+class ThirdPartyPayment {
+    constructor(id, amount) {
+        this.id = id;
+        this.amount = amount;
+    }
+    SendPayment() {
+        console.log(`3rd Party Payment Amount: ${this.amount} - ID: ${this.id}`);
+    }
+}
+var PaymentType;
+(function (PaymentType) {
+    PaymentType[PaymentType["ThirdParty"] = 0] = "ThirdParty";
+    PaymentType[PaymentType["Propriatery"] = 1] = "Propriatery";
+})(PaymentType || (PaymentType = {}));
+class PaymentAdapter {
+    constructor(id, total, paymentType) {
+        this.id = id;
+        this.total = total;
+        this.paymentType = paymentType;
+    }
+    SubmitPayment() {
+        if (this.paymentType === PaymentType.ThirdParty) {
+            const amount = this.total;
+            const id = parseInt(this.id);
+            const payment = new ThirdPartyPayment(id, amount);
+            payment.SendPayment();
+        }
+        else if (this.paymentType === PaymentType.Propriatery) {
+            const id = this.id.toString();
+            const payment = new Payment(id, this.total);
+            payment.SubmitPayment();
+        }
+        else {
+            throw new Error('Invalid payment type');
+        }
+    }
+}
 exports.default = () => {
     console.log(`${title}\n`);
+    const paymentA = new PaymentAdapter('aaa', 47.99, PaymentType.Propriatery);
+    paymentA.SubmitPayment();
+    const paymentB = new PaymentAdapter('bbb', 99.99, PaymentType.ThirdParty);
+    paymentB.SubmitPayment();
 };
--- src/notes01-design-patterns/structural/adapter-example02.ts
+++ src/notes01-design-patterns/structural/adapter-example02.ts
@@ -1,10 +1,5 @@
 const title = '->->Adapter payment exmaple';
 
-enum PaymentType {
-  ThirdParty,
-  Propietary,
-}
-
 interface IPayment {
   id: string;
   total: number;
@@ -23,6 +18,68 @@ class Payment implements IPayment {
   }
 }
 
+interface IThirdPartyPayment {
+  id: number;
+  amount: number;
+  SendPayment: Function;
+}
+
+class ThirdPartyPayment implements IThirdPartyPayment {
+  public id: number;
+  public amount: number;
+  constructor(id: number, amount: number) {
+    this.id = id;
+    this.amount = amount;
+  }
+  public SendPayment() {
+    console.log(`3rd Party Payment Amount: ${this.amount} - ID: ${this.id}`);
+  }
+}
+
+enum PaymentType {
+  ThirdParty,
+  Propriatery,
+}
+
+class PaymentAdapter implements IPayment {
+  public id: string;
+  public total: number;
+  public paymentType: PaymentType;
+
+  constructor(id: string, total: number, paymentType: PaymentType) {
+    this.id = id;
+    this.total = total;
+    this.paymentType = paymentType;
+  }
+
+  public SubmitPayment() {
+    if (this.paymentType === PaymentType.ThirdParty) {
+      const amount = this.total;
+      const id = parseInt(this.id);
+      const payment = new ThirdPartyPayment(id, amount);
+      payment.SendPayment();
+    } else if (this.paymentType === PaymentType.Propriatery) {
+      const id = this.id.toString();
+      const payment = new Payment(id, this.total);
+      payment.SubmitPayment();
+    } else {
+      throw new Error('Invalid payment type');
+    }
+  }
+}
+
 export default (): void => {
   console.log(`${title}\n`);
+  const paymentA: IPayment = new PaymentAdapter(
+    'aaa',
+    47.99,
+    PaymentType.Propriatery,
+  );
+  paymentA.SubmitPayment();
+  const paymentB: IPayment = new PaymentAdapter(
+    'bbb',
+    99.99,
+    PaymentType.ThirdParty,
+  );
+  paymentB.SubmitPayment();
 };
